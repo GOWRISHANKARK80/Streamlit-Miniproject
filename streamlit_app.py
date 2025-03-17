f7e53c6376be87e65c98da2a21c8ceb9a6ba2fb6
Gowri Shankar K <carthishank80@gmail.com>
2025-03-17 14:49:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ my_own_queries = {
         "Top 3 cities by revenue": 
             'SELECT o."city", SUM(d."list price" * d."quantity") AS revenue FROM df1_order o JOIN df1_orders d ON o."sub category" = d."sub category" GROUP BY o."city" ORDER BY revenue DESC LIMIT 3;',
         "Products with no profit": 
-            'SELECT "product id" FROM df1_orders GROUP BY "product id" HAVING SUM("profit") = 0;',
+            'SELECT "product id" FROM df1_orders GROUP BY "product id" HAVING SUM("profit") = 0 LIMIT 3;',
         "Top 3 countries with high sales by segment": 
             'SELECT "country", "segment", SUM("sales price") AS high_sales FROM df1_order o JOIN df1_orders d ON o."sub category" = d."sub category" GROUP BY o."country", o."segment" ORDER BY high_sales DESC LIMIT 3;'
 }
