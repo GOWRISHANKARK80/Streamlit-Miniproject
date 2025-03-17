8a58a5e15a97bce1e29ecebfced726b9084f43bd
Gowri Shankar K <carthishank80@gmail.com>
2025-03-17 15:03:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,8 +109,7 @@ my_own_queries = {
         "Top 3 cities by revenue": 
             'SELECT o."city", SUM(d."list price" * d."quantity") AS revenue FROM df1_order o JOIN df1_orders d ON o."sub category" = d."sub category" GROUP BY o."city" ORDER BY revenue DESC LIMIT 3;',
         "Top three Products with no profit": 
-            #'SELECT "product id" FROM df1_orders GROUP BY "product id" HAVING SUM("profit") = 0 LIMIT 3;',
-            'SELECT o."country", d."product id" FROM df1_orders d JOIN df1_order o ON d."sub category" = o."sub category" GROUP BY o."country", d."product id" HAVING SUM(d."profit") = 0 LIMIT 3;
+            'SELECT "product id" FROM df1_orders GROUP BY "product id" HAVING SUM("profit") = 0 LIMIT 3;',
         "Top 3 countries with high sales by segment": 
             'SELECT "country", "segment", SUM("sales price") AS high_sales FROM df1_order o JOIN df1_orders d ON o."sub category" = d."sub category" GROUP BY o."country", o."segment" ORDER BY high_sales DESC LIMIT 3;'
 }
@@ -317,7 +316,7 @@ if query:
     elif query == "Products with no profit":
         result_df = run_query(my_own_queries[query])
         if result_df is not None and not result_df.empty:
-            st.write("Products with No Profit:")
+            st.write("These are the products that have no profit:")
             st.write(result_df)
         else:
             st.write("All products are generating profit.")            
